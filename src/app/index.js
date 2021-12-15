2aa63f4edaa1954272e9593f4461e900f0f3e779
camilotrejos <c.juantrejos@gmail.com>
2021-12-15 08:13:54 -0500

    Initial

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,14 @@
-alert("probando alerta webpack")
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+class App extends Component{
+    render(){
+        return(
+            <h1>Hello World Class React</h1>
+        )
+    }
+}
+
+render(<App/>, document.getElementById("app"))
+
+
